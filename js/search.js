dc9982fd278d2cbe8fe9708643789191d0f75b96
elrizal <erin@lrizal.com>
2019-04-06 18:27:36 -0400

    refractor

--- js/search.js
+++ js/search.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		event.preventDefault();
 
 		var beer = $("#beer-input").val();
-		
+		console.log("beer = " + beer);
 		if (beer === "") {
 			var queryURL =
 				"https://api.punkapi.com/v2/beers/?page=2&per_page=60&" + beer;
@@ -23,7 +23,7 @@ $(document).ready(function() {
 			url: queryURL,
 			method: "GET"
 		}).done(function(response) {
-	
+			console.log(response);
 				if (response.indexOf(beer) == undefined){
 				var index = response.indexOf(beer)
 				var id = response.indexOf(beer) + 1
@@ -41,49 +41,36 @@ $(document).ready(function() {
 				beerFood = beerFood.split(",").join(", ");
 
 				//if we remove index, beerFood is now an array.
-				//This means to diplay all foods we need a separate loop and some separate jQ insode that loop
+				//This means to diplay all foods we need a separate loop and some separate Jquery insode that loop
 				//then we need to also get that into the modal, and we have packed it as a value in the data-food attribute
 				//however, again, since it's an array, when you deal with it in the new modal, you may have to loop again
 
-				var malt = response[i].ingredients.malt;
-				var hops = response[i].ingredients.hops;
-				var yeast = response[i].ingredients.yeast;
 				var beerBrewed = response[i].first_brewed;
 				var beerBrewery = response[i].withBreweries;
 				var beerOrganic = response[i].isOrganic;
 				var convertSearchterm = JSON.stringify(response);
 			
-				var beerCard = $("<li>");
-				var beerContent = $("<div>").addClass(beercontent);
+				var beerCard = $("<li>").css("display", "inline-flex", "float", "relative");
+				var beerContent = $("<div>").css("width", "250px", "height", "250px");
 
-				beerCard.addClass("collection-item").css("margin", "auto");
-				beerContent
-					.addClass("card-content gray-text")
-					.css("padding", "10px", "overflow", "hidden");
-				beerContent.attr("data-name", beerName);
-				beerContent.attr("data-describe", beerDescription);
-				beerContent.attr("data-abv", beerAbv);
-				beerContent.attr("data-year", beerBrewed);
-				beerContent.attr("data-food", beerFood);
+				beerCard.addClass("collection-item").css("margin", "20px");
+				beerContent.addClass("card-content gray-text")
+					.css("padding", "10px", "overflow", "hidden")
+					.attr("data-name", beerName)
+					.attr("data-describe", beerDescription)
+					.attr("data-abv", beerAbv)
+					.attr("data-year", beerBrewed)
+					.attr("data-brewery", beerBrewery)
+					.attr("data-food", beerFood);
 
-				var cardSpan = $("<span>");
-				cardSpan.addClass("card-title");
-				cardSpan.append("<i>" + beerTagline)
+					//building out the cards:
+				var cardSpan = $("<span>").addClass("card-title").append("<i>" + beerTagline)
 					.css({ color: "#1C1C1C", margin: "auto", padding: "5px" });
 
-				var cardText = $("<p>");
-				cardText.addClass("card-text");
+				var cardText = $("<p>").addClass("card-text");
 				cardSpan.html(beer);
 
-				var cardImage = $("<img>");
-				cardImage.addClass("card-image");
-				cardImage.css({
-					height: "250px",
-					display: "block",
-					margin: "auto",
-					padding: "8px"
-				});
-				cardImage.attr("src", beerImage);
+				var cardImage = $("<img>").addClass("card-image").attr("src", beerImage);
 				cardText.append(cardImage);
 
 				cardSpan.addClass("card-title").text(beerName);
@@ -92,144 +79,42 @@ $(document).ready(function() {
 					.append("<i>" + beerTagline)
 					.css({ color: "#a8a4b0", margin: "auto", padding: "5px" });
 
-				// var foodIcon = $("<i>").addClass("material-icons").text("add");
-				// cardText.append(foodIcon + "Great with: " + beerFood + "<br>").attr("material-icons", "local_dining");
-
 				beerContent.append(cardText);
 				beerCard.append(beerContent);
-				var dynamicCard = $("#list-stuff");
-				$("#list-stuff").append(beerCard);
-				beerCard.addClass("card").css("margin", "5px", "float", "relative");
-				// var beerRow = $("<div>");
-				//  beerRow.addClass("row").append(dynamicCard);
+				var dynamicCard = $("#list-stuff").append(beerCard);
+				beerCard.addClass("card")
 
 				//module trigger and adding the card classes
-				var trigger = $("<button>");
-				trigger.addClass("waves-effect waves-light btn");
-				trigger.attr("id", "trigger");
-				trigger.attr("data-target", "modal1");
-				trigger
-					.text("More Info")
-					.css({ display: "block", margin: "auto" });
+				var trigger = $("<button>").addClass("waves-effect waves-light btn").attr("id", "trigger", "data-target", "modal1");
+				trigger.text("More Info").css({ display: "block", margin: "auto" });
 				beerContent.append(trigger);
 
+				//Attaching the data of each beer found to pair with a card and modal:
 				$(".card").on("click", "button", function(event) {
-					var dataName = $(this)
-						.parent()
-						.attr("data-name");
-					var dataDescribe = $(this)
-						.parent()
-						.attr("data-describe");
-					var dataAbv = $(this)
-						.parent()
-						.attr("data-abv");
-					var dataYear = $(this)
-						.parent()
-						.attr("data-year");
-					var dataFood = $(this)
-						.parent()
-						.attr("data-food");
+					// var dataName = $(this).parent().attr("data-name");
+					// var dataDescribe = $(this).parent().attr("data-describe");
+					// var dataAbv = $(this).parent().attr("data-abv");
+					// var dataYear = $(this).parent().attr("data-year");
+					// var dataFood = $(this).parent().attr("data-food");
+					// var dataBrewery = $(this).parent().attr("data-brewery");
 
-					console.log("back of card beer " + dataName);
+					var beerInfo = {
+						dataName: $(this).parent().attr("data-name"),
+						dataDescribe: $(this).parent().attr("data-describe"),
+						dataAbv: $(this).parent().attr("data-abv"),
+						dataYear: $(this).parent().attr("data-year"),
+						dataFood: $(this).parent().attr("data-food"),
+						dataBrewery: $(this).parent().attr("data-brewery")
+					}
+
 					$("#modal1").modal("open");
-					$("#modal-header").html(dataName);	
-					$("#modal-body").html( 
-						dataDescribe + "<h5>" + "Alcohol by volume" + "</h5>" +
-							dataAbv + "%" + "</div>" + "<h5>" + "First Brewed on " + "</h5>" + 
-							dataYear + "<h5>" + "Goes best with: " + "</h5>" + 
-							dataFood 
+					$("#modal-header").html(beerInfo.dataName);	
+
+					//And finally, adding the data to the modal of each beer listed:
+					$("#modal-body").html( `${beerInfo.dataDescribe} <h5>Alcohol by Volume</h5> ${beerInfo.dataAbv}% </div> 
+					<h5>First Brewed On</h5> ${beerInfo.dataYear} <h5>Brewery</h5> ${beerInfo.dataBrewery} <h5>Goes Great With</h5> ${beerInfo.dataFood}`
 					);
-					$("#food-input").val(dataFood);
-					// NEED TO APPEND THIS  to the Container before the beer div <div id="modal1" class="modal modal-fixed-footer">
-					//   <div class="modal-content">
-					//     <h4>Modal Header</h4>
-					//     <p>A bunch of text</p>
-					//   </div>
-					//   <div class="modal-footer">
-					//     <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat "><i class="material-icons">close</i>Close</a>
-					//   </div>
-					// </div>
-				});
 
-				$("#find-food").on("click", function(event) {
-					event.preventDefault();
-
-					//now we've packed food array into value
-					var food = $("#food-input").val();
-
-					//loop through array to make dropdown
-
-					food = food.toString();
-					food = food.split(/[ ,]+/);
-
-					console.log("Food carried over: " + food);
-
-					var proxy = "https://cors-anywhere.herokuapp.com/";
-					var queryURL =
-						"https://food2fork.com/api/search?key=f18f20279482aabaea4a0d26c8810819&q=" +
-						food;
-
-					$.ajax({
-						url: proxy + queryURL,
-						method: "GET"
-					}).done(function(response) {
-						console.log(response);
-						console.log("hi");
-
-						var res = JSON.parse(response);
-						var recipes = res.recipes;
-						var count = res.count;
-						console.log(count);
-
-						if (count === 0) {
-							//alert("try again")
-							console.log("inside count === 0");
-							var noResults = $("<div>");
-							noResults.text("Try Again");
-							$("#food-view").append(noResults);
-						} else {
-							for (var i = 0; i < recipes.length; i++) {
-								var foodPublisher = recipes[i].publisher;
-								var foodTitle = recipes[i].title;
-								var foodSourceURL = recipes[i].source_url;
-								var foodImage = recipes[i].image_url;
-
-								// var foodDiv = $("#food-view");
-								var listItem = $("<li>");
-								listItem.addClass("food-item");
-								listItem.append(
-									"<h3>" +
-										foodTitle +
-										"</h3>" +
-										"<hr>" +
-										"PUBLISHER: " +
-										foodPublisher +
-										"<hr>"
-									// + "DESCRIPTION: " + foodSourceURL + "<hr>"
-								);
-
-								var source = $("<a>");
-								source.addClass("source");
-								source.text(
-									"Click here to get recipes for: " +
-										foodTitle
-								);
-								source.attr("href", foodSourceURL);
-
-								var image = $("<img>");
-								image.addClass("food-image");
-								image.attr("src", foodImage);
-
-								listItem.append(image);
-								listItem.append(source);
-
-								$("#food-view").append(listItem);
-												//FOR MAKING INVALID INPUTS:
-			
-				
-							}
-						}
-					});
 				});
 			}
 		});
